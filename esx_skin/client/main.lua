33c3b93c326cc08283388a40cf7078337b55167c
ZhoNNz <52080900+ZhoNNz@users.noreply.github.com>
2020-08-20 05:45:24 +0300

    Again Menu open fix

--- esx_skin/client/main.lua
+++ esx_skin/client/main.lua
@@ -10,6 +10,130 @@ Citizen.CreateThread(function()
 	end
 end)
 
+function OpenMenu(submitCb, cancelCb, restrict)
+	local playerPed = PlayerPedId()
+
+	TriggerEvent('skinchanger:getSkin', function(skin)
+		lastSkin = skin
+	end)
+
+	TriggerEvent('skinchanger:getData', function(components, maxVals)
+		local elements    = {}
+		local _components = {}
+
+		-- Restrict menu
+		if restrict == nil then
+			for i=1, #components, 1 do
+				_components[i] = components[i]
+			end
+		else
+			for i=1, #components, 1 do
+				local found = false
+
+				for j=1, #restrict, 1 do
+					if components[i].name == restrict[j] then
+						found = true
+					end
+				end
+
+				if found then
+					table.insert(_components, components[i])
+				end
+			end
+		end
+
+		-- Insert elements
+		for i=1, #_components, 1 do
+			local value       = _components[i].value
+			local componentId = _components[i].componentId
+
+			if componentId == 0 then
+				value = GetPedPropIndex(playerPed, _components[i].componentId)
+			end
+
+			local data = {
+				label     = _components[i].label,
+				name      = _components[i].name,
+				value     = value,
+				min       = _components[i].min,
+				textureof = _components[i].textureof,
+				zoomOffset= _components[i].zoomOffset,
+				camOffset = _components[i].camOffset,
+				type      = 'slider'
+			}
+
+			for k,v in pairs(maxVals) do
+				if k == _components[i].name then
+					data.max = v
+					break
+				end
+			end
+
+			table.insert(elements, data)
+		end
+
+		CreateSkinCam()
+		zoomOffset = _components[1].zoomOffset
+		camOffset = _components[1].camOffset
+
+		ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'skin', {
+			title    = _U('skin_menu'),
+			align    = 'top-left',
+			elements = elements
+		}, function(data, menu)
+			TriggerEvent('skinchanger:getSkin', function(skin)
+				lastSkin = skin
+			end)
+
+			submitCb(data, menu)
+			DeleteSkinCam()
+		end, function(data, menu)
+			menu.close()
+			DeleteSkinCam()
+			TriggerEvent('skinchanger:loadSkin', lastSkin)
+
+			if cancelCb ~= nil then
+				cancelCb(data, menu)
+			end
+		end, function(data, menu)
+			local skin, components, maxVals
+
+			TriggerEvent('skinchanger:getSkin', function(getSkin)
+				skin = getSkin
+			end)
+
+			zoomOffset = data.current.zoomOffset
+			camOffset = data.current.camOffset
+
+			if skin[data.current.name] ~= data.current.value then
+				-- Change skin element
+				TriggerEvent('skinchanger:change', data.current.name, data.current.value)
+
+				-- Update max values
+				TriggerEvent('skinchanger:getData', function(comp, max)
+					components, maxVals = comp, max
+				end)
+
+				local newData = {}
+
+				for i=1, #elements, 1 do
+					newData = {}
+					newData.max = maxVals[elements[i].name]
+
+					if elements[i].textureof ~= nil and data.current.name == elements[i].textureof then
+						newData.value = 0
+					end
+
+					menu.update({name = elements[i].name}, newData)
+				end
+
+				menu.refresh()
+			end
+		end, function(data, menu)
+			DeleteSkinCam()
+		end)
+	end)
+end
 
 function CreateSkinCam()
 	if not DoesCamExist(cam) then
@@ -166,39 +290,31 @@ AddEventHandler('esx_skin:setLastSkin', function(skin)
 	lastSkin = skin
 end)
 
-RegisterNetEvent('esx_skin:openMenu')
-AddEventHandler('esx_skin:openMenu', function(submitCb, cancelCb)
-	OpenMenu(submitCb, cancelCb, nil)
-end)
+-- RegisterNetEvent('esx_skin:openMenu')
+-- AddEventHandler('esx_skin:openMenu', function(submitCb, cancelCb)
+-- 	OpenMenu(submitCb, cancelCb, nil)
+-- end)
 
-RegisterNetEvent('esx_skin:openRestrictedMenu')
-AddEventHandler('esx_skin:openRestrictedMenu', function(submitCb, cancelCb, restrict)
-	OpenMenu(submitCb, cancelCb, restrict)
-end)
+-- RegisterNetEvent('esx_skin:openRestrictedMenu')
+-- AddEventHandler('esx_skin:openRestrictedMenu', function(submitCb, cancelCb, restrict)
+-- 	OpenMenu(submitCb, cancelCb, restrict)
+-- end)
 
-RegisterNetEvent('esx_skin:openSaveableMenu')
-AddEventHandler('esx_skin:openSaveableMenu', function(submitCb, cancelCb)
-	OpenSaveableMenu(submitCb, cancelCb, nil)
-end)
+-- RegisterNetEvent('esx_skin:openSaveableMenu')
+-- AddEventHandler('esx_skin:openSaveableMenu', function(submitCb, cancelCb)
+-- 	OpenSaveableMenu(submitCb, cancelCb, nil)
+-- end)
 
 RegisterNetEvent('esx_skin:setLastSkinNewAcc')
 AddEventHandler('esx_skin:setLastSkinNewAcc', function(NewAcc)
 	lastSkin = NewAcc
 end)
 
-RegisterNetEvent('esx_skin:karakterYukle')
-AddEventHandler('esx_skin:karakterYukle', function(xPlayer)
-	ESX.TriggerServerCallback('esx_skin:getPlayerSkin', function(skin, jobSkin)
-		if skin ~= nil then
-			TriggerEvent('skinchanger:loadSkin', skin)
-		end
-	end)
-end)
 
-RegisterNetEvent('esx_skin:openSaveableRestrictedMenu')
-AddEventHandler('esx_skin:openSaveableRestrictedMenu', function(submitCb, cancelCb, restrict)
-	OpenSaveableMenu(submitCb, cancelCb, restrict)
-end)
+-- RegisterNetEvent('esx_skin:openSaveableRestrictedMenu')
+-- AddEventHandler('esx_skin:openSaveableRestrictedMenu', function(submitCb, cancelCb, restrict)
+-- 	OpenSaveableMenu(submitCb, cancelCb, restrict)
+-- end)
 
 RegisterNetEvent('esx_skin:requestSaveSkin')
 AddEventHandler('esx_skin:requestSaveSkin', function()
